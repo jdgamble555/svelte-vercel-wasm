748a2b14dcb6a358c12d9aa11a9e8bdea81d5ec6
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-10 14:16:00 -0600

    satori

--- src/routes/og/+server.ts
+++ src/routes/og/+server.ts
@@ -1,6 +1,23 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { ImageResponse } from "@cf-wasm/og/others";
 import { t } from "@cf-wasm/og/html-to-react";
+import notoSansFontBuffer from '@cf-wasm/og/noto-sans-v27-latin-regular.ttf.bin';
+import yogaWasmModule from '@cf-wasm/satori/yoga.wasm?module';
+import { initSatori, initYoga } from '@cf-wasm/satori/others';
+import { initResvg } from '@cf-wasm/resvg/legacy/others';
+import resvgWasmModule from '@cf-wasm/resvg/legacy/resvg.wasm?module';
+
+if (!initSatori.initialized) {
+  initSatori(initYoga(yogaWasmModule));
+}
+
+if (!initResvg.initialized) {
+  initResvg(resvgWasmModule);
+}
+
+import { defaultFont } from '@cf-wasm/og/others';
+
+defaultFont.set(notoSansFontBuffer);
 
 export const GET: RequestHandler = async () => {
     const html = `<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 100%; height: 100%;">
