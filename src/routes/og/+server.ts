40ccee18d746f2e8eead8bbde304095265877c88
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-12 07:23:07 -0600

    others

--- src/routes/og/+server.ts
+++ src/routes/og/+server.ts
@@ -1,11 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { ImageResponse } from "@cf-wasm/og/others";
 import { t } from "@cf-wasm/og/html-to-react";
-import notoSansFontBuffer from '@cf-wasm/og/noto-sans-v27-latin-regular.ttf.bin';
-import yogaWasmModule from '@cf-wasm/satori/yoga.wasm?module';
+import notoSansFontBuffer from '@cf-wasm/og/others/noto-sans-v27-latin-regular.ttf.bin';
+import yogaWasmModule from '@cf-wasm/satori/others/yoga.wasm?module';
 import { initSatori, initYoga } from '@cf-wasm/satori/others';
-import { initResvg } from '@cf-wasm/resvg/legacy/others';
-import resvgWasmModule from '@cf-wasm/resvg/legacy/resvg.wasm?module';
+import { initResvg } from '@cf-wasm/resvg/others';
+import resvgWasmModule from '@cf-wasm/resvg/others/resvg.wasm?module';
 import { defaultFont } from '@cf-wasm/og/others';
 
 if (!initSatori.initialized) {
