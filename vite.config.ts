de6e1cb0159b5b9c06c8c796979b3125b910c748
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-21 08:15:29 -0600

    remove module

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import cloudflareModules from '@cf-wasm/plugins/vite-cloudflare-modules';
+//import cloudflareModules from '@cf-wasm/plugins/vite-cloudflare-modules';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
-		cloudflareModules({
+		/*cloudflareModules({
 			target: 'edge-light'
-		}),
+		}),*/
 	]
 });
