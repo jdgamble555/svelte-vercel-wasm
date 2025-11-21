22347355eed569ef85d970b9e577ed22186e65d7
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-21 08:13:07 -0600

    test

--- src/routes/og/+server.ts
+++ src/routes/og/+server.ts
@@ -1,6 +1,7 @@
 import { type RequestHandler } from "@sveltejs/kit";
-import { ImageResponse } from "$lib/image-response";
+//import { ImageResponse } from "$lib/image-response";
 import ImageCard from "$lib/image-card.svelte";
+import { ImageResponse } from "@ethercorps/sveltekit-og";
 
 
 export const prerender = false;
@@ -10,7 +11,7 @@ export const GET = (async ({ url }) => {
 
   const { width, height } = Object.fromEntries(url.searchParams);
 
-  return await ImageResponse(
+  return new ImageResponse(
     ImageCard,
     {
       width: Number(width) || 1600,
