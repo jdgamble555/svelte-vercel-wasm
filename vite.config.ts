68944ea54bfd5f48631a06184f2d53dba00327d1
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-21 08:21:41 -0600

    rollup

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 //import cloudflareModules from '@cf-wasm/plugins/vite-cloudflare-modules';
+import { rollupWasm } from '@ethercorps/sveltekit-og/plugin';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
+
 		/*cloudflareModules({
 			target: 'edge-light'
 		}),*/
-	]
+	],
+	build: {
+		rollupOptions: {
+			plugins: [rollupWasm()],
+		}
+	}
 });
